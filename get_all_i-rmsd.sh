d2eb05f08f1f63a08e0e8f6262c16ac03d80dd4b
RyanPendavingh <39589362+RyanPendavingh@users.noreply.github.com>
2019-12-10 14:05:30 +0100

    Delete get_all_i-rmsd.sh

--- get_all_i-rmsd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in `seq 1 200`
-do
-	profit -f i-rmsd.txt -h /Users/ryan/Documents/Data/case10_refcomp_struc.ent \
-	/Users/ryan/Documents/case10_XL/structures/it1/water/complex_${i}w.pdb \
-	| grep "RMS:" | cut -f5 -d' ' | tail -n1 >> \
-	/Users/ryan/Documents/case10_XL/results/all_i-rmsd.txt
-done
